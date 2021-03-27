4fe508863c277564d9ee5da45027c44bc52d8afb
Jan Prochazka <jan.prochazka@gmail.com>
2021-03-27 16:09:35 +0100

    support big files on read

--- src/backend/reader.js
+++ src/backend/reader.js
@@ -15,27 +15,45 @@ async function loadWorkbook(fileName) {
   return workbook;
 }
 
+async function waitForDrain(stream) {
+  return new Promise(resolve => {
+    stream.once('drain', () => {
+      resolve();
+    });
+  });
+}
+
 async function reader({ fileName, sheetName, limitRows = undefined }) {
+  const pass = new stream.PassThrough({
+    objectMode: true,
+    highWaterMark: 100
+  });
+
   const workbook = await loadWorkbook(fileName);
   const sheet = workbook.Sheets[sheetName];
 
-  const pass = new stream.PassThrough({
-    objectMode: true,
+  const rows = xlsx.utils.sheet_to_json(sheet, {
+    header: 1, blankrows: false
   });
-  const rows = xlsx.utils.sheet_to_json(sheet, { header: 1 });
   const header = rows[0];
   const structure = {
     columns: _.range(header.length).map((index) => ({ columnName: header[index] })),
   };
-  pass.write(structure);
-  for (let rowIndex = 1; rowIndex < rows.length; rowIndex++) {
-    if (limitRows && rowIndex > limitRows) break;
-    const row = rows[rowIndex];
-    const rowData = _.fromPairs(structure.columns.map((col, index) => [col.columnName, row[index]]));
-    if (_.isEmpty(_.omitBy(rowData, (v) => v == null || v.toString().trim().length == 0))) continue;
-    pass.write(rowData);
+  if (!pass.write(structure)) await waitForDrain(pass);
+
+  const sendAsync = async () => {
+    for (let rowIndex = 1; rowIndex < rows.length; rowIndex++) {
+      if (limitRows && rowIndex > limitRows) break;
+      const row = rows[rowIndex];
+      const rowData = _.fromPairs(structure.columns.map((col, index) => [col.columnName, row[index]]));
+      if (_.isEmpty(_.omitBy(rowData, (v) => v == null || v.toString().trim().length == 0))) continue;
+      if (!pass.write(rowData)) await waitForDrain(pass);
+    }
+    pass.end();
   }
-  pass.end();
+
+  // don't wait for sending
+  sendAsync();
 
   return pass;
 }
